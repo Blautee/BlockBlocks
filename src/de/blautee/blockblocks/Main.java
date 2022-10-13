167e1aff621f1c5ca8e930c6b14daff90b00e4b6
Blautee <blautee0@gmail.com>
2022-10-13 23:05:49 +0200

    structure

---
+++ src/de/blautee/blockblocks/Main.java
@@ -0,0 +1,7 @@
+package de.blautee.blockblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+}
---
+++ src/de/blautee/blockblocks/PlaceListener.java
@@ -0,0 +1,7 @@
+package de.blautee.blockblocks;
+
+import org.bukkit.event.Listener;
+
+public class PlaceListener implements Listener {
+
+}
